0428cfcf7d86c1412070c1b49b2b0da892746a7b
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-07-31 21:39:27 -0400

    created handlebars layout page

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 const path = require('path');
 const express = require('express');
-const exhbs = require('express-handlebars');
-const routes = require('./controllers');
-//add utils file
-const helpers = require('./utils')
-const sequelize = require('sequelize');
+const exphbs = require('express-handlebars');
+//const routes = require('./controllers');
+//const helpers = require('./utils/helpers')
+//const sequelize = require('./config/connection');
+//const { application } = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exhbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+//app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT} >🚀`));
+//testing home page
+app.get('', (req, res) => {
+  res.render('home');
 })
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening on port ${PORT} >🚀`));
+// })
+
+app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT} >🚀`));
