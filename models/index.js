673fba91a0e8a6c84aa95238e54c87626f2afcb2
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-08-02 01:07:51 -0400

    created comment.hbs file

--- models/index.js
+++ models/index.js
@@ -1,3 +1,16 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+//Associations
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+
+module.exports = { User, Post };
